98ef3e565273abdd0f94990d144af3a4cd37a543
sergiolmartinez <smart0586@gmail.com>
2023-03-16 23:21:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const MAPI_KEY = process.env.API_KEY;
+const MLIST_ID = process.env.LIST_ID;
+const MAPI_SERVER = process.env.API_SERVER;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url =
+    "https://" + MAPI_SERVER + ".api.mailchimp.com/3.0/lists/" + MLIST_ID;
+
+  const options = {
+    method: "POST",
+    auth: "smart0586:" + MAPI_KEY,
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+  // console.log(firstName, lastName, email);
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running...");
+});
+
+// API Key
+//
+
+// List ID
+//
